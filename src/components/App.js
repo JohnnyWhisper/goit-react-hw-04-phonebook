26b1b6be3f9f23a3e8119b5a9fdb33393409a594
JohnnyWhisper <92794546+JohnnyWhisper@users.noreply.github.com>
2022-07-26 01:32:03 +0300

    refactoring

---
+++ src/components/App.js
@@ -0,0 +1,74 @@
+import { useState, useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
+
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm';
+import ContactFilter from './ContactFilter';
+import ContactList from './ContactList';
+import Container from './Container';
+
+const App = () => {
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('Contacts')) ?? []
+  );
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const contacts = localStorage.getItem('contacts');
+    const parcedContacts = JSON.parse(contacts);
+
+    if (parcedContacts) {
+      setContacts(parcedContacts);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = ({ name, number }) => {
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    const alreadyFind = contacts.find(contact => contact.name === name);
+
+    alreadyFind
+      ? toast.error(`${name} is already in contacts.`)
+      : setContacts(prevState => [contact, ...prevState]);
+  };
+
+  const findContact = event => {
+    setFilter(event.target.value);
+  };
+
+  const getFilterContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const deleteContact = contactId => {
+    setContacts(contacts.filter(contact => contact.id !== contactId));
+  };
+  return (
+    <Container>
+      <h1 className="Title">Phonebook</h1>
+      <ContactForm onSubmit={addContact} />
+      <h2 className="Title">Contacts</h2>
+      <ContactFilter value={filter} onFindContact={findContact} />
+      <ContactList
+        onFilteredContacts={getFilterContacts()}
+        onDeleteContact={deleteContact}
+      />
+      <ToastContainer />
+    </Container>
+  );
+};
+
+export default App;
---
+++ src/components/ContactFilter/ContactFilter.js
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import styles from './ContactFilter.module.css';
+
+const ContactFilter = ({ value, onFindContact }) => {
+  return (
+    <label className={styles.Label}>
+      Find contacts by name
+      <input
+        type="text"
+        value={value}
+        className={styles.Input}
+        onChange={onFindContact}
+      />
+    </label>
+  );
+};
+
+ContactFilter.propTypes = {
+  value: PropTypes.string,
+  onFindContact: PropTypes.func.isRequired,
+};
+
+export default ContactFilter;
